cfbe901156a2b6674d81edd3d65571a4714c1131
jmwesigwa272-ai <joshuamwesigwa300@gmail.com>
2026-07-14 10:16:42 +0300

    Ajusted index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello, World! Node is working...');
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => { // new
+  res.send('Homepage! Hello world.');
+});
+
+app.get('/about', (req, res) => { // new
+  res.send('About page. Nice.');
+});
+
+app.listen(3000, () => console.log('listening on port 3000')); // Always the last line of the file.
